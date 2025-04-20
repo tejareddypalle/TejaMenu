4928fab31b904a31c4659e9da6113bd32a1f3c8d
TEJA REDDY PALLE <168373029+tejareddypalle@users.noreply.github.com>
2025-04-19 21:36:37 -0500

    Add files via upload

---
+++ nutrition.js
@@ -0,0 +1,31 @@
+function chepatiNutrition() {
+  alert("Chapati\nProtein: 5g\nCalories: 120\nFat: 3g\nCarbohydrates: 20g");
+}
+
+function DosaNutrition() {
+  alert("Dosa\nProtein: 4g\nCalories: 150\nFat: 6g\nCarbohydrates: 22g");
+}
+
+function MomoNutrition() {
+  alert("Momo\nProtein: 8g\nCalories: 180\nFat: 7g\nCarbohydrates: 18g");
+}
+
+function PooriNutrition() {
+  alert("Poori\nProtein: 3g\nCalories: 200\nFat: 10g\nCarbohydrates: 25g");
+}
+
+function IdlyNutrition() {
+  alert("Idly\nProtein: 2g\nCalories: 60\nFat: 0.5g\nCarbohydrates: 13g");
+}
+
+function UpmaNutrition() {
+  alert("Upma\nProtein: 4g\nCalories: 180\nFat: 5g\nCarbohydrates: 30g");
+}
+
+function VadaNutrition() {
+  alert("Vada\nProtein: 3g\nCalories: 150\nFat: 8g\nCarbohydrates: 15g");
+}
+
+function BondaNutrition() {
+  alert("Bonda\nProtein: 2g\nCalories: 190\nFat: 9g\nCarbohydrates: 20g");
+}
